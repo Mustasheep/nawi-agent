be87223487792278503ea579c7f2dc25cba0dfb2
Thiago <thiagoassis.escritorio@gmail.com>
2026-03-16 18:19:43 -0300

    feat: remove arquivos do agente de documentação, incluindo core, templates e exemplos, para simplificação do projeto

--- core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Core modules for the documentation agent
-"""
-
-from .claude_client import ClaudeClient
-from .file_scanner import FileScanner
-from .context_builder import ContextBuilder
-
-__all__ = ['ClaudeClient', 'FileScanner', 'ContextBuilder']
